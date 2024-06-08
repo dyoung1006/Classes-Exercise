46efec4b52df83fbd991161105f8355656bc7bc2
Damien Young <dyoung1006@gmail.com>
2024-06-08 07:07:34 -0700

    inital commit

---
+++ Classes/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public Car(string make, string model, int year) 
+        {
+            Make = make;
+            Model = model;  
+            Year = year;            
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }   
+        public int Year { get; set; }
+
+        public void DisplayCarDetails()
+        {
+            Console.WriteLine(Make);
+            Console.WriteLine(Model);
+            Console.WriteLine(Year);
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Classes
+﻿using System.Reflection;
+
+namespace ClassesExercise
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myCar = new Car("Jeep", "Cherokee", 2019);
+            myCar.DisplayCarDetails();
+            Console.ReadKey();
         }
     }
 }
+
+#region - Instructions
+//Create a class named Car - make sure it is public
+//In the Car class:
+//Create a Make property of type string that is public
+//Create a Model property of type string that is public
+//Create a Year property of type int that is public
+//In the Main method:
+//Create a new instance of the Car class
+//Set values in the properties for the object
+//Print the values of each property to the Console
+#endregion
